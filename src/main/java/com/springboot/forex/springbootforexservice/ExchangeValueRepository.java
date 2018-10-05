013841b82c005d8acd67bf34aef3ba3346568621
Vinoth <vinoth186@gmail.com>
2018-10-05 13:16:13 +0530

    Spring boot Forex microservice

---
+++ src/main/java/com/springboot/forex/springbootforexservice/ExchangeValueRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.forex.springbootforexservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long>
+{
+    ExchangeValue findByFromAndTo(String from, String to);
+}
---
+++ src/main/java/com/springboot/forex/springbootforexservice/SpringBootForexServiceApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.forex.springbootforexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootForexServiceApplication.class, args);
+	}
+}
